eb3f3ce091b71b0726ede823e17249fcd9dcc8e5
sanceo1994-rgb <san.ceo1994@gmail.com>
2026-06-27 14:47:40 +0900

    ci: add test1 verification workflow

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,18 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      "dist-ssr",
+      ".output",
+      ".vinxi",
+      ".tanstack",
+      ".nitro",
+      ".vercel",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
